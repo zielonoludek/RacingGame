2bc6069b57e25a6f87fb86f64b1530558561a907
Martyna Cichosz <martynacichosz2@gmail.com>
2023-09-25 15:18:40 +0200

    car rotates and doesnt jump

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,6 +8,7 @@ public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody rb;
     private float speed = 10f;
+    private float rotationSpeed = 125f;
     private ActionsEditor playerActions;
 
     private void Awake()
@@ -17,24 +18,21 @@ public class PlayerMovement : MonoBehaviour
         playerActions = new ActionsEditor();
         playerActions.Player.Enable();
 
-        playerActions.Player.Jump.performed += Jump;
         playerActions.Player.Movement.performed += Movement;
     }
 
     private void Update()
     {
         Vector2 inputVector = playerActions.Player.Movement.ReadValue<Vector2>();
-        rb.AddForce(new Vector3(inputVector.x, 0, inputVector.y) * speed, ForceMode.Force);
+        Vector3 movement = new Vector3(inputVector.x, 0, inputVector.y);
+        rb.AddForce(movement * speed, ForceMode.Force);
+
+        if (movement != Vector3.zero)   transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(movement), rotationSpeed * Time.deltaTime);
     }
 
     private void Movement(InputAction.CallbackContext contex)
     {
         Vector2 inputVector = contex.ReadValue<Vector2>();
         rb.AddForce(new Vector3(inputVector.x, 0, inputVector.y) * speed, ForceMode.Force);
-    }
-
-    public void Jump(InputAction.CallbackContext contex)
-    { 
-        rb.AddForce(Vector3.up * 5f, ForceMode.Impulse);
     }
 }
