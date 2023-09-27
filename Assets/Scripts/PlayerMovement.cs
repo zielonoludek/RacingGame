ab490b56f832f2901e63c2a88dd4c618534adca1
Pablo Ariel Nicoli <pablo.nicoli@magicmedia.studio>
2023-09-27 13:25:57 +0400

    First clean up PlayerMovement.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,18 +4,10 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public int playerNum;
-
     private PlayerInput playerInput;
     [SerializeField] private float speed = 10f;
 
-    private Rigidbody rb;
-
-    private void Awake()
-    {
-        rb = GetComponent<Rigidbody>();
-        playerInput = GetComponent<PlayerInput>();
-    }
+    private void Awake() => playerInput = GetComponent<PlayerInput>();
 
     private void Update()
     {
@@ -23,11 +15,5 @@ public class PlayerMovement : MonoBehaviour
 
         transform.Translate(0, 0, speed * Time.deltaTime);
         transform.Rotate(0, movement.x, 0);
-    }
-
-    private void Movement(InputAction.CallbackContext contex)
-    {
-        Vector2 inputVector = contex.ReadValue<Vector2>();
-        rb.AddForce(new Vector3(inputVector.x, 0, inputVector.y) * speed, ForceMode.Force);
     }
 }
