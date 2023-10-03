4072c91c656ae20f6bea03536b32ee2182c0b1c3
zielonoludek <martynacichosz2@gmail.com>
2023-10-03 15:42:02 +0200

    road prefabs

---
+++ Assets/Scripts/Managers/UIManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private GameObject mainMenuPanel;
+    [SerializeField] private GameObject menuPanel;
+
+    void Start()
+    {
+        mainMenuPanel.SetActive(true);
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            menuPanel.SetActive(true);
+        }
+    }
+}
